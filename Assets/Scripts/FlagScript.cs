5dd3e6b6efa661c9f41f5297feb4da107a690cb9
mahoneycon99 <mahoneycon99@gmail.com>
2018-09-27 18:52:25 -0400

    Controller Scripts
    Basic controller tracking scripts that attach a hammer to one hand and a flag to the other.

---
+++ Assets/Scripts/FlagScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagScript : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    public GameObject flagPrefab;
+    private GameObject flag;
+    private FixedJoint joint;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+    // Use this for initialization
+    void Start () {
+        flag = Instantiate(flagPrefab);
+        joint = gameObject.AddComponent<FixedJoint>();
+        joint.connectedBody = flag.GetComponent<Rigidbody>();
+        joint.breakForce = 10000;
+        joint.breakTorque = 10000;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/HammerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerScript : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    public GameObject hammerPrefab;
+    private GameObject hammer;
+    private FixedJoint joint;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        hammer = Instantiate(hammerPrefab);
+        joint = gameObject.AddComponent<FixedJoint>();
+        joint.connectedBody = hammer.GetComponent<Rigidbody>();
+        joint.breakForce = 10000;
+        joint.breakTorque = 10000;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
